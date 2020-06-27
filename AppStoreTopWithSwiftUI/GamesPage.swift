8ce4adf2eaaec6dca2d945a14bfa08f0b4347600
park kyung suk <boardguy1024@gmail.com>
2020-06-27 14:42:32 +0900

    Perfome baseView

---
+++ AppStoreTopWithSwiftUI/GamesPage.swift
@@ -0,0 +1,23 @@
+//
+//  GamesPage.swift
+//  AppStoreTopWithSwiftUI
+//
+//  Created by park kyung suk on 2020/06/27.
+//  Copyright © 2020 park kyung suk. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct GamesPageView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct GamesPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        GamesPageView()
+    }
+}
+
